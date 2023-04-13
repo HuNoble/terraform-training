97379c2ddcbce84c540dce3a25fc1497bb07137e
Ábel Szabó <abel.szao@devoteam.com>
2023-04-13 22:56:49 +0200

    version bump

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
       prefix = "terraform-training-"
     }
   }
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.4.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
